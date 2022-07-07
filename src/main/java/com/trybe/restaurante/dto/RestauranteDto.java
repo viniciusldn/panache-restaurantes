58d9d9ed6d6c007c3fd86d67d4d5bd4706b7433f
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-07-07 10:14:14 -0300

    :construction: RestauranteDto
    criada

--- src/main/java/com/trybe/restaurante/dto/RestauranteDto.java
+++ src/main/java/com/trybe/restaurante/dto/RestauranteDto.java
@@ -1,5 +1,8 @@
 package com.trybe.restaurante.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * Classe RestauranteDto.
  **/
@@ -9,9 +12,30 @@ public class RestauranteDto {
   /**
    * Atributos.
    */
+  @NotBlank
+  private String nome;
+
+  @NotBlank
+  @Size(min = 3)
+  private String descricao;
 
   /**
    * Métodos.
    */
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
 
 }
